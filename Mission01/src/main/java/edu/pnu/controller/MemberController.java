e273d16635abd8ad277886237b349d488c07594b
KEN <shdnrnjs@gmail.com>
2023-03-23 01:02:29 +0900

    mission end

--- Mission01/src/main/java/edu/pnu/controller/MemberController.java
+++ Mission01/src/main/java/edu/pnu/controller/MemberController.java
@@ -3,8 +3,11 @@ package edu.pnu.controller;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pnu.edu.domain.Member;
@@ -19,26 +22,44 @@ public class MemberController {
 		System.out.println("MemberController 생성");
 	}
 	
-	
+	// 1. get 방식. service에서 데이터 저장하고 여기서 호출. -- read 영역
 	@GetMapping("/Member")
-	public List<Member> getMembers(){
-		
-		
-		
-		return m1.getMember();
+	public List<Member> getMember1(){
+		return m1.getMember1();
 	}
 	
+	// 2. url에 변수 입력 방식 -- read 영역
 	//@PathVariable은 url경로에 변수 넣어주는 것
-//	@RequestMapping(value = "/Member/{id}", method = RequestMethod.GET)
-	
-	@GetMapping(path="/Member/{id}")
+	@GetMapping("/Member/{id}")
 	public Member getMember2(@PathVariable int id) {
-		
 		return m1.getMember2(id);
 		
 	}
 	
-	//나머지는 집가서 하기
+	// 3. 폼에서 입력 받으면 데이터 들어옴 -- create 영역
+	@PostMapping("/Member")
+	public Member addMember(Member m) {
+		System.out.println("폼에서 입력받은 값 : " + m1.toString());
+		return m1.addMember(m);
+	}
+
+	// 4. put -- update 영역
+	@PutMapping("/Member")
+	public Member updateMember(Member m) {
+		System.out.println("member");
+		
+		return m1.updateMember(m);
+	}
+	
+	//근데 이렇게 하면 2번이랑 겹치지 않나?. 지우려니까 추가한 값만 지워지네?
+	// 5. delete
+	@DeleteMapping("/Member/{id}")
+	public Member deleteMember(@PathVariable int id) {
+		System.out.println("삭제");
+		
+		return m1.deleteMember(id);
+	}
+	
 	
 	
 	
--- Mission01/src/main/java/pnu/edu/service/MemberService.java
+++ Mission01/src/main/java/pnu/edu/service/MemberService.java
@@ -16,23 +16,19 @@ public class MemberService {
 	List<Member> list = new ArrayList<>();
 	Member m = new Member();
 	
+	//생성자 - 객체 생성될 때마다 데이터 리스트에 삽입
 	public MemberService() {
-		list.add(new Member(1, "1234", "홍길동", new Date()));
-		list.add(new Member(2, "1235", "홍길동1", new Date()));
-		list.add(new Member(3, "1236", "홍길동2", new Date()));
-		list.add(new Member(4, "1237", "홍길동3", new Date()));
-		list.add(new Member(5, "1238", "홍길동4", new Date()));
-	
-			
+		for(int i=1; i<=5; i++) {
+			list.add(new Member(i,"1234","hong",new Date()));
+		}
 	}
 	
-	public List<Member> getMember(){
-		
-		
+	// 리스트에 저장된 데이터 반환하는 함수
+	public List<Member> getMember1(){
 		return list;
 	}
 	
-	
+	// url에 입력된 변수가 list에 저장된 데이터와 일치하면 리턴
 	public Member getMember2(int id) {
 		
 		for(Member m : list) {
@@ -42,10 +38,44 @@ public class MemberService {
 		}
 		
 		return null;
-		
-		
-		
 	}
 	
+	// create
+	public Member addMember(Member m) {
+		
+		int id = m.getId();
+		String pass = m.getPass();
+		String name = m.getName();
+		Date d = new Date();
+		m = new Member(id, pass, name, d);
+		list.add(m);
+		return m;
+	}
+	
+	//update
+	public Member updateMember(Member m) {
+		
+		for(Member m1 : list) {
+			if(m1.getId() == m.getId()) {
+				m1.setPass(m.getPass());
+				m1.setName(m.getName());
+				return m1;
+			}
+		}
+		
+		return null;
+	}
+	
+	//delete
+	public Member deleteMember(int id) {
+		for(Member m1 : list) {
+			if(m1.getId()== id) {
+				list.remove(m1.getId());
+				return m1;
+			}
+		}
+		
+		return null;
+	}
 	
 }
