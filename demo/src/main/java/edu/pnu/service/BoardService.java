e503fb99164031849c069866fd996a781f00b0d0
KEN ROE <shdnrnjs@gmail.com>
2023-03-22 13:53:09 +0900

    new

---
+++ demo/src/main/java/edu/pnu/service/BoardService.java
@@ -0,0 +1,15 @@
+package edu.pnu.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pnu.domain.Person;
+
+public class BoardService {
+	
+	private List<Person> list = new ArrayList<>();
+	
+	
+	
+
+}
