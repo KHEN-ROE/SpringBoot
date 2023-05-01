02214def63b13e447226dab4bf597033432a0c57
KEN ROE <73014470+KHEN-ROE@users.noreply.github.com>
2023-05-01 12:02:39 +0900

    Delete TodoList-1 directory

--- TodoList-1/src/main/java/edu/pnu/persistence/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.pnu.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pnu.edu.domain.Todo;
-
-public interface TodoRepository extends CrudRepository<Todo, Long> {
-}
