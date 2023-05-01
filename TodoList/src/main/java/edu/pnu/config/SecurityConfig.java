ad424d2ff26a89df3b0366b294d5c96f622a4cb6
KEN <shdnrnjs@gmail.com>
2023-05-01 12:01:42 +0900

    making login

---
+++ TodoList/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+//package edu.pnu.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity // spring security 설정을 활성화 시켜줌
+//public class SecurityConfig {
+//	//시큐리티 6.0으로
+//}
---
+++ TodoList2/src/main/java/edu/pnu/TodoList2Application.java
@@ -0,0 +1,13 @@
+package edu.pnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoList2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoList2Application.class, args);
+	}
+
+}
